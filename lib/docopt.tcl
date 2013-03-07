c2eaf7b8ee00e80046cd6875120875867b6bf43d
Arthur Schreiber <schreiber.arthur@googlemail.com>
2013-03-07 21:51:04 +0100

    Start rewriting for Tcl 8.6.

--- lib/docopt.tcl
+++ lib/docopt.tcl
@@ -1,19 +1,112 @@
 namespace eval Docopt {
-    proc docopt { doc args } {
+    oo::class create Pattern {
 
-        if { [llength $args] > 3 } {
-            error "too many args"
+    }
+
+    oo::class create ChildPattern {
+        superclass Pattern
+    }
+
+    oo::class create ParentPattern {
+        superclass Pattern
+    }
+
+    oo::class create Argument {
+        superclass ChildPattern
+    }
+
+    oo::class create Command {
+        superclass Argument
+    }
+
+    oo::class create Option {
+        superclass ChildPattern
+
+        self method parse { option_description } {
+            set short {}
+            set long {}
+            set argcount 0
+            set value {}
+
+            regexp -indices -- "  " [string trim $option_description] pos
+
+            set options [string range $option_description 0 [lindex $pos 0]-1]
+            set description [string range $option_description [lindex $pos 1]+1 "end"]
+
+            foreach s [split $options "=, "] {
+                if { [string range $s 0 1] == "--" } {
+                    set long $s
+                } elseif { [string range $s 0 0] == "-" }  {
+                    set short $s
+                } else {
+                    set argcount 1
+                }
+            }
+
+            if { $argcount > 0 } {
+                set matched [regexp -inline -all -nocase -- {\[default: (.*)\]} $description]
+                if { [llength $matched] > 0 } {
+                    set value [lindex $matched 1]
+                }
+            }
+
+            my new $short $long $argcount $value
         }
 
-        set version [expr { [llength $args] == 3 ? [lindex $args 2] : "" }]
-        set help    [expr { [llength $args] == 2 ? [lindex $args 1] : false }]
-        set args    [expr { [llength $args] == 1 ? [lindex $args 0] : $::argv }]
+        constructor { short long argcount value } {
+            set [self]::short $short
+            set [self]::long $long
+            set [self]::argcount $argcount
+            set [self]::value $value
+        }
+    }
 
-        return ""
+    oo::class create Required {
+        superclass ParentPattern
+    }
+
+    oo::class create Optional {
+        superclass ParentPattern
+    }
+
+    oo::class create AnyOptions {
+        superclass Optional
+    }
+
+    oo::class create OneOrMore {
+        superclass ParentPattern
+    }
+
+    oo::class create Either {
+        superclass ParentPattern
+    }
+
+    oo::class create TokenStream {
+
+    }
+
+    # long ::= '--' chars [ ( ' ' | '=' ) chars ] ;
+    proc parse_long { tokens options } {
+
+    }
+
+    proc parse_defaults { doc } {
+        set matches [regexp -all -inline {\n *(<\S+?>|-\S+?)} $doc]
+
+        set options [list]
+
+        foreach {s1 s2} $matches {
+            set str "$s1$s2"
+            if { [string range $str 0 1] == "-" } {
+                lappend options [Option parse $str]
+            }
+        }
+
+        return $options
     }
 
     proc printable_usage { doc } {
-        set matches [regexp -all -nocase -indices -inline {usage:} $doc]
+        set matches [regexp -all -nocase -indices -inline "usage:" $doc]
 
         if { [llength $matches] == 0 } {
             return -code error -errorcode "DocoptLanguageError" "\"usage:\" (case-insensitive) not found."
@@ -47,53 +140,7 @@ namespace eval Docopt {
         join $result " "
     }
 
-    proc parse_doc_options { doc } {
-        set result ""
+    proc docopt { doc { argv {} } { help true } { version {} } { options_first false } } {
 
-        foreach line [split $doc "\n"] {
-            if { [regexp {^ *-} $line] } {
-                lappend result [Option::parse $line]
-            }
-        }
-
-        return $result
-    }
-
-    namespace eval Option {
-        proc parse { description } {
-            set result [dict create "argcount" "0"]
-
-            set description [string trim $description]
-
-            if { [set seperator [string first "  " $description]] > -1 } {
-                set options [string range $description 0 $seperator]
-                set description [string range $description $seperator+2 end]
-            } else {
-                set options $description
-                set description ""
-            }
-
-            set options [string map {"=" " " "," " "} $options]
-
-            foreach option $options {
-                if { [string range $option 0 1] == "--" } {
-                    dict set result "long" $option
-                } elseif { [string range $option 0 0] == "-" } {
-                    dict set result "short" $option
-                } else {
-                    dict set result "argcount" 1
-                }
-            }
-
-            if { [regexp -nocase {\[default: (.*)\]} $description -> default] } {
-                dict set result "default" $default
-            }
-
-            if { [dict exists $result "long"] || [dict exists $result "short"] } {
-                return $result
-            } else {
-                return ""
-            }
-        }
     }
 }
